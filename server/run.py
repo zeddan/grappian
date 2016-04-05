09130eff5d1eab72bab7d1942680212a21cc79d4
emil petersson <emil petersson>
2016-04-05 15:14:24 +0200

    Gets a list from echonest.py and adds to playlist

--- server/run.py
+++ server/run.py
@@ -80,5 +80,27 @@ def create_playlist():
 def root():
     return static_file('index.html', root='./public')
 
-
+#set tmp_username and tmp_playlist to match your account details
+@route('/add-songs')
+def add_songs():
+    echo_object = echonest.get_casual('rock', 'happy')
+    songs = []
+    for songid in echo_object:
+        songs.append(songid['song_id'])
+    song_list = {"uris": songs}
+    access_token = request.get_cookie('access_token')
+    refresh_token = request.get_cookie('refresh_token')
+    if access_token:
+            pass
+    elif refresh_token:
+            access_token, expires_in = auth.refresh_token(refresh_token)
+            response.set_cookie('access_token',
+                                access_token,
+                                max_age=expires_in,
+                                path='/')
+    else:
+            redirect('/authorize')
+    tmp_username = 'emilh4xx'
+    tmp_playlist = '1E6fK83UsFzMC1aomxbgm1'
+    return spotify.add_songs_to_playlist(access_token, tmp_username, tmp_playlist, song_list)
 run(host='0.0.0.0', port=8080, debug=True, reloader=True)
