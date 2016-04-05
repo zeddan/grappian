ba80421985ad30e77da41679c263b603bb05b6c3
emil petersson <emil petersson>
2016-04-05 15:25:40 +0200

    Refactored the comments

--- server/spotify.py
+++ server/spotify.py
@@ -7,16 +7,18 @@ def me(access_token):
     headers = {'Authorization': 'Bearer %s' % access_token}
     return requests.get(url, headers=headers).json()
 
-#change what is in the brackets to fit your account
+
 def create_playlist(access_token, name):
+    """ Change what is in the brackets to fit your account"""
     url = 'https://api.spotify.com/v1/users/emilh4xx/playlists'
     headers = {'Authorization': 'Bearer %s' % access_token,
                'Content-Type': 'application/json'}
     params = {'name': name}
     return requests.post(url, headers=headers, data=json.dumps(params)).json()
 
-#fix what is in the brackets to fit your account and playlist
+
 def add_songs_to_playlist(access_token, user_id, playlist_id, track_list):
+    """ Fix what is in the brackets to fit your account and playlist """
     base_url = 'https://api.spotify.com/v1/users'
     purpose_url = '/%s/playlists/%s/tracks' % (user_id, playlist_id)
     headers = {'Authorization': 'Bearer %s' % access_token,
