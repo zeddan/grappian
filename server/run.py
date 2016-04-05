99bc9c7b8766e75b8e4581d3a6d47c8820980d1b
emil petersson <emil petersson>
2016-04-05 15:27:08 +0200

    Refactored some comments

--- server/run.py
+++ server/run.py
@@ -83,7 +83,7 @@ def root():
 
 @route('/add-songs')
 def add_songs():
-    """ Set tmp_username and tmp_playlist to match your account details """
+    """Set tmp_username and tmp_playlist to match your account details"""
     echo_object = echonest.get_casual('rock', 'happy')
     songs = []
     for songid in echo_object:
