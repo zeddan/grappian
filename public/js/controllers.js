c31aded0e7ea960036dc9e4688e989b3a6182278
Robin Sääf <robin.saaf@chas.se>
2016-04-07 13:35:31 +0200

    it's now possible to choose mood/genre and click submit. next step is to get it to talk to the backend

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,7 +4,38 @@
     var app = angular.module('controllers', []);
 
     app.controller('ModesController', ['$scope', function($scope) {
-        $scope.modes = ['happy', 'sad', 'angry'] 
+        $scope.titles = {
+            'modes': 'modes',
+            'preferences': 'preferences',
+            'casual': {
+                'genre': 'genre',
+                'mood': 'mood'
+            }
+        };
+        $scope.modes = ['casual', 'theme', 'expert'];
+        $scope.preferences = {
+            'casual': {
+                'genres': ['dub acid', 'jazz'],
+                'moods': ['happy', 'angry']
+            },
+            'theme': {
+            },
+            'expert': {
+            }
+        };
+        $scope.selected = {
+            'mode': '',
+            'params': {}
+        };
+        $scope.selectGenre = function(index) {
+            $scope.selected.params.genre = $scope.preferences.casual.genres[index];
+        };
+        $scope.selectMood = function(index) {
+            $scope.selected.params.mood = $scope.preferences.casual.moods[index];
+        };
+        $scope.submit = function() {
+            console.log($scope.selected.params);
+        };
     }]);
 
 }());
