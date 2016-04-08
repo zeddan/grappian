2fe5bf3461905b4191ee7f5b5cc536c5a85339f3
Kalk88 <simon.d.mansson@gmail.com>
2016-04-08 16:24:43 +0200

    updated services with 2 factorys

--- public/js/services.js
+++ public/js/services.js
@@ -2,25 +2,11 @@
     'use strict';
     var app = angular.module('services', []);
 
-    app.factory('Data' , function() {
-        var data =
-        {
-            playlistId: '',
-            echonestList: ''
-        };
-        return {
-            getPlaylistId: function() {
-                return data.playlistId;
-            },
-            setPlaylistID: function(playlistId) {
-                data.playlistId = playlistId;
-            },
-            getEchonestList: function() {
-                return data.echonestList;
-            },
-            setEchonestList: function(echonestList) {
-                data.echonestList = echonestList;
-            }
-        };
+    app.factory('echonestList' , function() {
+        return {echonestList :''};
     });
-});
+
+    app.factory('playlistId', function() {
+        return {playlistId: ''};
+    });
+}());
