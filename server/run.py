fccecf9388f2bd6414d8be67f5181b30e75d1b4c
jienna <jhedaker@gmail.com>
2016-04-20 10:36:15 +0200

    dots

--- server/run.py
+++ server/run.py
@@ -38,7 +38,7 @@ def enable_cors():
 
 @route('/public/<path:re:.+>')
 def static(path):
-    return static_file(path, root='./public')
+    return static_file(path, root='../public')
 
 
 @route('/authorize')
